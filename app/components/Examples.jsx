122d28cb4f9399a576e1644aa350f2b2d4766665
philnichols <philnichols@desq.co.uk>
2016-06-15 15:06:19 +0100

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
 var Examples = (props) => {
 
-        return (
+    return (
+        <div>
             <h3>Examples Component! </h3>
-        )
+            <p>Welcome</p>
+        </div>
+    )
 
 };
 
