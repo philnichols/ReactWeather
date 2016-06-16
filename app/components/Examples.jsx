564e83c04beb9a509dbae590148d61db8ac7dbb4
philnichols <philnichols@desq.co.uk>
2016-06-16 10:49:33 +0100

    Foundation styles added

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 var Examples = (props) => {
 
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Hera are a  few example locations to try out</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Sheffield' >Sheffield, UK</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Rio' >Rio, Brazil</Link>
+                </li>
+                <li></li>
+            </ol>
         </div>
     )
 
