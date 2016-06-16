195cf3193e9a9cec5c582d4cc0a99312cabd3fd3
philnichols <philnichols@desq.co.uk>
2016-06-16 14:26:04 +0100

    Add nav search support

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,16 @@ var Nav = React.createClass({
 
     onSearch: function (e) {
         e.preventDefault();
-        alert("Not wired up yet!");
+
+        var location = this.refs.search.value;
+
+        var encodedLocation = encodeURIComponent(location);
+
+        if (location.length > 0) {
+            this.refs.search.value = '';
+
+            window.location.hash = '#/?location=' +  encodedLocation;
+        }
 
     },
 
@@ -29,7 +38,7 @@ var Nav = React.createClass({
                     <form onSubmit={this.onSearch}>
                         <ul className="menu">
                             <li>
-                                <input type="search" placeholder="Search weather by city" />
+                                <input type="search" placeholder="Search weather by city" ref="search" />
                             </li>
                             <li>
                                 <input type="submit" className="button" value="Get Weather" />
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -4,21 +4,23 @@ var WeatherMessage = require('WeatherMessage');
 var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
 var Weather = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             isLoading: false
         }
     },
-    handleSearch: function(location){
+    handleSearch: function (location) {
         var that = this;
 
         this.setState({
             isLoading: true,
-            errorMessage: undefined
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         });
 
         openWeatherMap.getTemp(location).then(
-            function(temp){
+            function (temp) {
                 that.setState(
                     {
                         location: location,
@@ -27,7 +29,7 @@ var Weather = React.createClass({
                     }
                 );
             },
-            function(e) {
+            function (e) {
 
                 that.setState(
                     {
@@ -36,22 +38,41 @@ var Weather = React.createClass({
                     }
                 );
             }
-
         )
     },
+    componentDidMount: function () {
+        var location = this.props.location.query.location;
+
+        if(location && location.length >0) {
+            this.handleSearch(location);
+            window.location.hash = "#/";
+        }
+
+    },
+
+    componentWillReceiveProps: function(newProps) {
+
+        var location = newProps.location.query.location;
+
+        if(location && location.length >0) {
+            this.handleSearch(location);
+            window.location.hash = "#/";
+        }
+    }
+    ,
     render: function () {
         var {isLoading, temp, location, errorMessage}  = this.state;
 
-        function renderMessage(){
-            if(isLoading) {
+        function renderMessage() {
+            if (isLoading) {
                 return <h3>Fetching Weather... </h3>;
             } else if (temp && location) {
                 return <WeatherMessage temp={temp} location={location} />;
             }
         }
 
-        function renderError () {
-            if(typeof  errorMessage === 'string'){
+        function renderError() {
+            if (typeof  errorMessage === 'string') {
                 return (
                     <ErrorModal message={errorMessage}/>
                 )
